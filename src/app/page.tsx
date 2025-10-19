f1905fa4c5594cd1ca7124b400a724a40ceef742
Mohmad Al Sallomi <mhmd.salomi.123@gmail.com>
2025-10-19 16:19:29 +0300

    app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 import Header from '@/components/sections/header';
 import HeroSection from '@/components/sections/hero';
-import AboutHeroSection from '@/components/sections/about-hero';
-import ServicesHeroSection from '@/components/sections/services-hero';
 import StatsBanner from '@/components/sections/stats-banner';
 import AboutSection from '@/components/sections/about';
 import ServicesSection from '@/components/sections/services';
 import StatisticsSection from '@/components/sections/statistics';
+import Section3 from '@/components/sections/section3';
 import ProcessSection from '@/components/sections/process';
 import ProjectsSection from '@/components/sections/projects';
 import TestimonialsSection from '@/components/sections/testimonials';
 import PanoramaSection from '@/components/sections/panorama';
 import TeamSection from '@/components/sections/team';
 import CtaSection from '@/components/sections/cta';
-import BlogSection from '@/components/sections/blog';
 import GallerySection from '@/components/sections/gallery';
 import Footer from '@/components/sections/footer';
 
@@ -24,16 +22,12 @@ export default function HomePage() {
       <main>
         <HeroSection />
         <StatsBanner />
-        <AboutSection />
-        <ServicesSection />
+        <Section3 />
+        <TeamSection />
+
         <StatisticsSection />
         <ProcessSection />
         <ProjectsSection />
-        <TestimonialsSection />
-        <PanoramaSection />
-        <TeamSection />
-        <CtaSection />
-        <BlogSection />
         <GallerySection />
       </main>
       
